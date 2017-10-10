61ae80ae750204f9f7e2ddec79a76585915d7d92
neutmute <neutmute@users.noreply.github.com>
2017-10-10 14:40:50 +1100

    options

--- src/Jotter.Scenarios/Interfaces/IScenarioOutput.cs
+++ src/Jotter.Scenarios/Interfaces/IScenarioOutput.cs
@@ -3,7 +3,9 @@
     public interface IScenarioOutput
     {
         JwtScenario Scenario { get; }
-        
+
+        IJwtBuildOptions BuildOptions { get; set; }
+
         string Token { get; set; }
     }
 }
