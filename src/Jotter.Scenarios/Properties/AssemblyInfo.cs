6df722c512d15199443137a0d981f3491b83f95a
neutmute <neutmute@users.noreply.github.com>
2017-10-10 14:45:10 +1100

    ass info

---
+++ src/Jotter.Scenarios/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Jotter.Scenarios")]
+[assembly: AssemblyDescription("JWT Scenario and Token creation")]
+
+[assembly: AssemblyCompany("neutmute")]
+[assembly: AssemblyProduct("Jotter")]
+[assembly: ComVisible(false)]
+
+
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("Developer Build")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
